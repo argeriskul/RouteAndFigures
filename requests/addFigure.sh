a7d5d83ea66406cae063adb600d0b20f739bf0ed
Dmitriy_Argeriskul <argeriskul@infobis.ru>
2018-04-03 10:26:30 +0400

    added missed files

---
+++ requests/addFigure.sh
@@ -0,0 +1,2 @@
+curl -X PUT -H "Content-Type: application/json" localhost:9000/addFigure -d '{"vertexList":[{"x":1,"y":2},{"x":20,"y":10}]}'
+
---
+++ requests/checkRoute.sh
@@ -0,0 +1,1 @@
+curl -X POST -H "Content-Type: application/json" localhost:9000/checkRoute -d '{"checkpoints":[{"x":3,"y":1},{"x":4,"y":3}, {"x":10,"y":10}]}'
